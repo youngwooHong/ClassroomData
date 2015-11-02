73d1fca6904aed2aaac5647af07e47d7b49fd9c2
SonarBoy <Joshua.L99.C@gmail.com>
2015-11-02 15:40:02 -0500

    first message

--- MVCApplication/WebContent/PersonalLoanController.jsp
+++ MVCApplication/WebContent/PersonalLoanController.jsp
@@ -2,11 +2,29 @@
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>Calculate Period</title>
+	</head>
+	
+	
+	<body>
+	
+		<form action="LoanController" method="get">
+			<p>Ammount Borrowed:<input type="number" name="ammountBorrowed"></input></p>
+			<p>Payment Frequency: <select name="paymentFrequency">
+	  			<option value="Weekly">Weekly</option>
+	  			<option value="Bi-Weekly">Bi-Weekly</option>
+	  			<option value="Semi-Monthly">Semi-Monthly</option>
+	  			<option value="Monthly">Monthly</option>
+			</select> </p>
+			<p>Interest Rate:<input type="number" name="interestRate"></p>
+			<p>Ammoritization Period<input type="number" name="period"></p>
+			<input type="submit" value="Submit!"></input>
+		</form>
+		
+	
+	
+	</body>
+	
 </html>
--- MVCApplication/src/LoanController.java
+++ MVCApplication/src/LoanController.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +28,57 @@ public class LoanController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Ammount Borrowed: " + request.getParameter("ammountBorrowed"));
+		out.println("PaymentFrequency: " + this.finder(request.getParameter("paymentFrequency")+""));
+		out.println("Interest Rate: " + request.getParameter("interestRate"));
+		out.println("Period: " + request.getParameter("period"));
+		out.println("GET");
+		
+		PersonalLoanCalculator calc = new PersonalLoanCalculator();
+		calc.setAmmountBorrowd(Integer.parseInt(request.getParameter("ammountBorrowed")));
+		calc.setPaymentFrequency(this.finder(request.getParameter("paymentFrequency")));
+		calc.setInterestRate(Double.parseDouble(request.getParameter("interestRate")));
+		calc.setTimePeriod(Integer.parseInt(request.getParameter("period")));
+		
+		out.println("Answer: " + calc.getAnswer());
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Ammount Borrowed: " + request.getParameter("ammountBorrowed"));
+		out.println("PaymentFrequency: " + this.finder(request.getParameter("paymentFrequency")+""));
+		out.println("Interest Rate: " + request.getParameter("interestRate"));
+		out.println("Period: " + request.getParameter("period"));
+		out.println("POST");
+		
+		PersonalLoanCalculator calc = new PersonalLoanCalculator();
+	}
+	
+	private int finder(String value){
+		
+		switch(value){
+			
+		case "Weekly":
+			return 52;
+		case "Bi-Weekly":
+			return 36;
+		case "Semi-Monthly":
+			return 24;
+		case "Monthly":
+			return 12;
+		default:
+			return 0;
+		}
+		
 	}
 
 }
